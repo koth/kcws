e254fa7309b47580f3af4a764c384f1adf2d0b75
Koth <chenyw@inmindglobal.com>
2016-11-29 21:55:07 +0800

    add 1998 pku corpus processing

---
+++ kcws/cc/gen_seg_eval.cc
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016- 2018 Koth. All Rights Reserved.
+ * =====================================================================================
+ * Filename:  gen_seg_eval.cc
+ * Author:  Koth
+ * Create Time: 2016-11-29 09:26:39
+ * Description:
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <chrono>
+
+#include "base/base.h"
+#include "utils/basic_string_util.h"
+
+
+#include "tf_seg_model.h"  //NOLINT
+#include "sentence_breaker.h"  // NOLINT
+#include "tensorflow/core/platform/init_main.h"
+
+DEFINE_string(test_file, "", "the test file");
+DEFINE_string(model_path, "", "the model path");
+DEFINE_string(vocab_path, "", "vocab path");
+
+DEFINE_int32(max_setence_len, 80, "max sentence len");
+
+const int BATCH_SIZE = 2000;
+int load_test_file(const std::string& path,
+                   std::vector<std::string>* pstrs) {
+  FILE *fp = fopen(path.c_str(), "r");
+  if (fp == NULL) {
+    VLOG(0) << "open file error:" << path;
+    return 0;
+  }
+  char line[4096] = {0};
+  int tn = 0;
+  while (fgets(line, sizeof(line) - 1, fp)) {
+    int nn = strlen(line);
+    while (nn && (line[nn - 1] == '\n' || line[nn - 1] == '\r')) {
+      nn -= 1;
+    }
+    if (nn <= 0) {
+      continue;
+    }
+    pstrs->push_back(std::string(line, nn));
+    tn += 1;
+  }
+  fclose(fp);
+  return tn;
+}
+int main(int argc, char *argv[]) {
+  tensorflow::port::InitMain(argv[0], &argc, &argv);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  if (FLAGS_vocab_path.empty()) {
+    VLOG(0) << "basic bocab path is not set";
+    return 1;
+  }
+  if (FLAGS_model_path.empty()) {
+    VLOG(0) << " model path is not set";
+    return 1;
+  }
+  if (FLAGS_test_file.empty()) {
+    VLOG(0) << " test_file path is not set";
+    return 1;
+  }
+  FILE* outfp = fopen("out_eval.txt", "w");
+  CHECK(outfp != nullptr) << "open file 'out_eval.txt' error";
+  kcws::TfSegModel sm;
+  CHECK(sm.LoadModel(FLAGS_model_path,
+                     FLAGS_vocab_path,
+                     FLAGS_max_setence_len))
+      << "Load model error";
+
+  std::vector<std::string> teststrs;
+  int ns = load_test_file(FLAGS_test_file, &teststrs);
+  std::string todo;
+  for (int i = 0; i < ns; i++) {
+    todo.append(teststrs[i]);
+  }
+  VLOG(0) << "loaded :" << FLAGS_test_file << " ,got " << ns << " lines";
+
+  auto start = std::chrono::steady_clock::now();
+  for (int i = 0; i < ns; i++) {
+    VLOG(0) << "do line:" << i;
+    if (teststrs[i].empty()) {
+      VLOG(0) << "empty line , continue";
+      continue;
+    }
+    std::vector<std::string> results;
+    CHECK(sm.Segment(teststrs[i], &results)) << "segment error";
+    int nr = results.size();
+    CHECK_NE(nr, 0);
+    fprintf(outfp, "%s", results[0].c_str());
+    for (int i = 1; i < nr; i++) {
+      fprintf(outfp, " %s", results[i].c_str());
+    }
+    fprintf(outfp, "\n");
+  }
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
+                  (std::chrono::steady_clock::now() - start);
+  VLOG(0) << "spend " << duration.count() << " milliseconds for file:" << FLAGS_test_file;
+
+  return 0;
+}
---
+++ kcws/train/process_people.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# @Author: Koth
+# @Date:   2016-11-29 09:20:36
+# @Last Modified by:   Koth
+# @Last Modified time: 2016-11-29 15:58:30
+
+import sys
+import w2v
+
+SEQ_LEN = 80
+
+
+def processToken(x, y, tok, vob):
+  if len(tok) == 1:
+    x.append(vob.GetWordIndex(str(tok[0].encode("utf8"))))
+    y.append(0)
+  else:
+    nn = len(tok)
+    for i in range(nn):
+      x.append(vob.GetWordIndex(str(tok[i].encode("utf8"))))
+      if i == 0:
+        y.append(1)
+      elif i == (nn - 1):
+        y.append(3)
+      else:
+        y.append(2)
+
+
+def processFile(inp, oup, mode, vob):
+  global SEQ_LEN
+  while True:
+    line = inp.readline()
+    if not line:
+      break
+    line = line.strip()
+    if not line:
+      continue
+    ss = line.split("  ")
+    oline = ""
+    x = []
+    y = []
+    for s in ss:
+      pos = s.find("/")
+      if not pos:
+        print("fatal error '/' not found")
+        sys.exit(0)
+      s = s[:pos]
+      ustr = unicode(s.decode("utf-8"))
+      if len(ustr) < 1:
+        continue
+      if mode == 0:
+        for i in range(len(ustr)):
+          oline += str(ustr[i].encode("utf8"))
+          oline += " "
+      else:
+        processToken(x, y, ustr, vob)
+    if mode != 0:
+      nn = len(x)
+      for i in range(nn, SEQ_LEN):
+        x.append(0)
+        y.append(0)
+      for i in range(SEQ_LEN):
+        oline += str(x[i]) + " "
+      for i in range(SEQ_LEN):
+        oline += str(y[i]) + " "
+    olen = len(oline)
+    oline = oline[:olen - 1]
+    oup.write("%s\n" % (oline))
+
+
+def main(argc, argv):
+  if argc < 3:
+    print(
+        "Usage: %s <input>  <output> [model | 0 for w2v , 1 for training]  [vec_path | if mode if not 0]"
+        % (argv[0]))
+    sys.exit(1)
+  mode = 0
+  vob = None
+  if argc > 4:
+    mode = int(argv[3])
+    vob = w2v.Word2vecVocab()
+    vob.Load(argv[4])
+  inp = open(argv[1], "r")
+  oup = open(argv[2], "w")
+  processFile(inp, oup, mode, vob)
+
+
+if __name__ == '__main__':
+  main(len(sys.argv), sys.argv)
